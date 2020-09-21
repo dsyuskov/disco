346c09b8c905ba8948328a938a2d14ff27d00355
int_21h <yuskovss@gmail.com>
2020-09-21 23:35:43 +0400

    parse newlook

---
+++ src/components/product.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="product">
+    <img class="product__photo" :src="product.photo" :alt="product.name" />
+    <div class="product__name">{{product.name}}({{product.sku}})</div>
+    <div class="product__quantity">{{product.quantity}}</div>
+    <div class="product__price">{{product.price}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: ["product"]
+};
+</script>
+
+<style scoped>
+.product {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  height: 100px;
+  border: 1px solid green;
+}
+
+.product__photo {
+  box-sizing: border-box;
+  width: 100px;
+  height: 100px;
+  border: 1px solid red;
+}
+</style>
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,19 +1,49 @@
 <template>
-  <hello-world />
+  <div>
+    <Product v-for="product in products" v-bind:product="product" v-bind:key="product.sku" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+// import newlook from "../newlook.json";
+import Product from "@/components/product.vue";
 
 export default {
   name: "App",
-  components: { HelloWorld }
+  components: { Product },
+  data: () => {
+    return {
+      products: [],
+      result: null
+    };
+  },
+  created() {
+    axios("https://www.newlook.com/uk/json/cart/currentCart.json").then(
+      response => {
+        console.log(response);
+        response.data.data.entries.forEach(item => {
+          console.log("item", item);
+          this.products.push({
+            name: item.product.name,
+            price: item.basePrice.formattedValue,
+            totalPrice: item.totalPrice.formattedValue,
+            sku: item.product.sku,
+            quantity: item.quantity,
+            photo: "https:" + item.product.imageUrl
+          });
+        });
+      }
+    );
+  }
 };
 </script>
 
 <style>
 html {
-  width: 400px;
+  box-sizing: border-box;
+  padding: 10px;
+  width: 500px;
   height: 400px;
 }
 </style>
